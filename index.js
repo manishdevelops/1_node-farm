60714a8c5987ff519eef1d6cf602f27b8861a222
manish mandal <76776194mm@gmail.com>
2023-09-23 17:15:53 +0530

    created a simple server

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const fs = require('fs');
+const http = require('http');
+const url = require('url');
 
+///////////////////////////
+/////FILES
 //Blocking, synchronous way
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 // console.log(textIn);
@@ -13,17 +17,51 @@ const fs = require('fs');
 // });
 // console.log('first execution');
 
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-	if (err) return console.log('ERROR! :⚡⚡');
-	fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-		console.log(data2);
-		fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
-			console.log(data3);
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+// 	if (err) return console.log('ERROR! :⚡⚡');
+// 	fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+// 		console.log(data2);
+// 		fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+// 			console.log(data3);
 
-			fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
-				console.log('Your file has been written🙂');
-			});
+// 			fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
+// 				console.log('Your file has been written🙂');
+// 			});
+// 		});
+// 	});
+// });
+// console.log('first execution');
+
+/////////////////////////////////////
+
+// SERVER
+
+//sync bcz it will only be executed at the starting time only
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+	// console.log(req);
+	// console.log(req.url);
+	const pathName = req.url;
+	if (pathName === '/' || pathName === '/overview') {
+		res.end('This is the OVERVIEW');
+	} else if (pathName === '/product') {
+		res.end('This is the PRODUCT');
+	} else if (pathName === '/api') {
+		res.writeHead(200, {
+			'Content-type': 'application/json',
 		});
-	});
+		res.end(data);
+	} else {
+		res.writeHead(404, {
+			'content-type': 'text/html',
+			'my-own-header': 'hello-world',
+		});
+		res.end('<h1>Page not found!</h1>');
+	}
 });
-console.log('first execution');
+
+server.listen(8000, '127.0.0.1', () => {
+	console.log('Listening to requests on port 8000');
+});
